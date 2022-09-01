d23c0664bcd49043052fd8169b8f79090b1188ed
omar auf <38477776+omarauf@users.noreply.github.com>
2022-09-02 01:36:11 +0300

    fucvj

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,7 +38,7 @@ try {
 
 	server.on('listening', () => {
 		logger.info(
-			`node server is listening on port ${env.NODE_PORT} in ${env.NODE_ENV} mode run by ${env.NODE_RUN_BY} v: ${env.NODE_VERSION} newnewn`
+			`node server is listening on port ${env.NODE_PORT} in ${env.NODE_ENV} mode run by ${env.NODE_RUN_BY} v: ${env.NODE_VERSION} fucker it`
 		);
 	});
 
