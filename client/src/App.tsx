2c0bc2383b962c69bc85d495b03a5ec4e60260ef
omar auf <38477776+omarauf@users.noreply.github.com>
2022-09-02 20:08:25 +0300

    new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
   console.log(
     `App is running in ${import.meta.env.VITE_ENV} mode run by ${import.meta.env.VITE_RUN_BY} v: ${
       import.meta.env.VITE_V
-    } version`,
+    } version `,
   );
 
   useEffect(() => {
